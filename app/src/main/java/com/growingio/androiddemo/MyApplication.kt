e2edcb976cec7901c6b81d17425172826a997ff6
Liz <lizwangying@icloud.com>
2018-10-22 21:00:14 +0800

    first commit

---
+++ app/src/main/java/com/growingio/androiddemo/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.growingio.androiddemo
+
+import android.app.Application
+
+/**
+ * classDesc:
+ */
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
