f3a4bd48a7547d7ccfb31e7b585d9e3786ca4005
xiangyang <xiangyang@growingio.com>
2019-01-24 19:04:57 +0800

    [feature] 接入触达SDK

--- app/src/main/java/com/growingio/giodemo/GIOApplication.kt
+++ app/src/main/java/com/growingio/giodemo/GIOApplication.kt
@@ -1,13 +1,21 @@
 package com.growingio.giodemo
 
 import android.app.Application
+import android.util.Log
 import com.growingio.android.sdk.collection.Configuration
 import com.growingio.android.sdk.collection.GrowingIO
+import com.growingio.android.sdk.gtouch.GrowingTouch
+import com.growingio.android.sdk.gtouch.config.GTouchConfig
+import com.growingio.android.sdk.gtouch.listener.EventPopupListener
 
 /**
  * classDesc: Application , 初始化 GrowingIO SDK
  */
+
+const val TAG: String = "GIOApplication"
+
 class GIOApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
 
@@ -17,6 +25,43 @@ class GIOApplication : Application() {
                 .setTestMode(true)
                 .setDebugMode(true)
                 .setChannel("XXX应用商店")
+        )
+        GrowingIO.getInstance().userId = "GIOXiaoYing"
+
+        GrowingTouch.startWithConfig(
+            this, GTouchConfig()
+                .setDebugEnable(true)
+                .setEventPopupEnable(true)
+                .setEventPopupShowTimeout(3000)
+                .setServerHost("http://k8s-marketing-automation-messages.growingio.com")
+                .setEventPopupListener(object : EventPopupListener {
+                    override fun onLoadFailed(
+                        eventId: String?,
+                        eventType: String?,
+                        errorCode: Int,
+                        description: String?
+                    ) {
+                        Log.e(TAG, "onLoadFailed: eventId = $eventId, eventType = $eventType， description = $description")
+                    }
+
+                    override fun onCancel(eventId: String?, eventType: String?) {
+                        Log.e(TAG, "onCancel: eventId = $eventId, eventType = $eventType")
+                    }
+
+                    override fun onLoadSuccess(eventId: String?, eventType: String?) {
+                        Log.e(TAG, "onLoadSuccess: eventId = $eventId, eventType = $eventType")
+                    }
+
+                    override fun onClicked(eventId: String?, eventType: String?, openUrl: String?): Boolean {
+                        Log.e(TAG, "onClicked: eventId = $eventId, eventType = $eventType openUrl = $openUrl")
+                        return false
+                    }
+
+                    override fun onTimeout(eventId: String?, eventType: String?) {
+                        Log.e(TAG, "onTimeout: eventId = $eventId, eventType = $eventType")
+                    }
+
+                })
         )
 
     }
