eff317bb1e634c364093f11df76707d4945644f6
wangying <wangying@growing.io>
2019-03-19 14:01:37 +0800

    [feature]: add DeepLink callback jump to product detail page

--- app/src/main/java/com/growingio/giodemo/GIOApplication.kt
+++ app/src/main/java/com/growingio/giodemo/GIOApplication.kt
@@ -1,9 +1,11 @@
 package com.growingio.giodemo
 
 import android.app.Application
+import android.content.Intent
 import android.util.Log
 import com.growingio.android.sdk.collection.Configuration
 import com.growingio.android.sdk.collection.GrowingIO
+import com.growingio.android.sdk.deeplink.DeeplinkCallback
 import com.growingio.android.sdk.gtouch.GrowingTouch
 import com.growingio.android.sdk.gtouch.config.GTouchConfig
 import com.growingio.android.sdk.gtouch.listener.EventPopupListener
@@ -25,6 +27,15 @@ class GIOApplication : Application() {
                 .setTestMode(BuildConfig.DEBUG)
                 .setDebugMode(BuildConfig.DEBUG)
                 .setChannel(BuildConfig.CHANNEL)
+//              DeepLink 直达商品详情页面，注意：自定义参数的接收与建立 DeepLink 唤醒链接时设置一致
+                .setDeeplinkCallback { params: MutableMap<String, String>, errorCode: Int ->
+                    if (errorCode == DeeplinkCallback.SUCCESS && params["JumpTo"] != null) {
+                        startActivity(
+                            Intent(this, ProductDetailActivity::class.java)
+                                .putExtra("product", params["JumpTo"])
+                        )
+                    }
+                }
         )
         GrowingIO.getInstance().userId = "GIOXiaoYing"
 
