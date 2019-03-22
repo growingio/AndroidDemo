65040003ebb38a783b789de7198f7c4d798acba9
wangying <wangying@growing.io>
2019-03-22 15:24:41 +0800

    [feature]:添加批量上传和获得埋点事件脚本

--- createEvents/README.md
+++ createEvents/README.md
@@ -1,2 +1,41 @@
-# GrowingIOAutoTrack
-GrowingIO 批量打点
+# GrowingIO API Script
+
+## 1. 功能介绍
+批量导出和导入 GrowingIO 的自定义事件和变量
+  
+## 2. 使用步骤  
+* ### 环境准备  
+    python3 下载和安装，请自行百度， 友情贴出链接   
+    windows : https://www.jianshu.com/p/7a0b52075f70  
+    mac : https://pythonguidecn.readthedocs.io/zh/latest/starting/install3/osx.html 
+
+* ### 下载脚本  
+    会使用 Git 的同学， 可以直接 clone 本项目，
+    不会使用 Git 的同学， 点击下载。
+
+    下载完成脚本后，执行以下操作安装依赖库：
+    ```
+    # 如果目录文件名是 GrowingIOAutoTrack-master
+    cd GrowingIOAutoTrack-master 
+    pip install pandas
+    pip install requests
+    ```
+
+* ### 配置文件填写    
+    编辑文件 config.ini，填写文件中对应字段。  
+
+    字段获取方式：  
+|  字段名称   	|   获取方式	|
+|---	|---	|
+|   project_id	|官网登陆后->右上角设置小图标-项目概览->左侧基本信息中的“项目ID”|
+|   publicKey	|同上项目概览页面，复制“项目公钥”|
+|   privateKey	|同上项目概览页面，复制“项目私钥”|
+|   project_uid	|点击GrowingIO Logo，进入“首页”，复制url中对应字段-> https://www.growingio.com/projects/复制这里的字符串/homepage/overview	|
+* ### 执行脚本
+
+    ```
+    python3 ./createEvents.py
+    ```
+
+* ### 上传自定义事件表格命名对应事件
+    
--- createEvents/createEvents.py
+++ createEvents/createEvents.py
@@ -1,167 +1,212 @@
-#-*- coding=utf-8 -*-
-import emoji
+# -*- coding=utf-8 -*-
 import pandas as pd
 import hashlib
 import hmac
 import time
 import requests as req
 import json
+import configparser
+import os
 
-project_id = '97fd6815651f25fb'
-publicKey = '06b3f5eb364f410eb7435a6940c4f431'
-privateKey = '3bde47c09c534a0db10a4b2402d3cd46'
-project_uid= 'z98jGyZP'
+current_path = os.path.dirname(os.path.realpath(__file__))
+config_path = os.path.join(current_path, "config.ini")
 
-# 创建事件级变量，将事件级变量 csv 文件名填入，默认 events.csv
-cstmFileName = ''
-# 创建事件，将您的自定义事件文件名输入，默认 cstmEvents.csv
-cstmEventsFileName = ''
-# 创建页面级变量
-pvarFileName = ''
-# 创建登录用户变量
-pplFileName = ''
+conf = configparser.ConfigParser()
+conf.read(config_path, encoding="utf-8")  
+sections = conf.sections()
 
-def authToken(secret, project, ai, tm):
+project_id = conf.get('GrowingIOProjectInfo', "project_id")
+project_key = conf.get('GrowingIOProjectInfo', "project_key")
+private_key = conf.get('GrowingIOProjectInfo', "private_key")
+project_uid = conf.get('GrowingIOProjectInfo', "project_uid")
+
+def auth_token(secret, project, ai, tm):
     """计算 auth """
     message = ("POST\n/auth/token\nproject=" + project + "&ai=" + ai + "&tm=" + tm).encode('utf-8')
     signature = hmac.new(bytes(secret.encode('utf-8')), bytes(message), digestmod=hashlib.sha256).hexdigest()
     return signature
 
-def getToken():
-    tmStamp = str(round(time.time() * 1000))
-    print ('使用时间戳 tm -> '+tmStamp)
 
+def get_token():
+    tm_stamp = str(round(time.time() * 1000))
     # 私钥、project uid , ai , tm
-    authStr = authToken(privateKey, project_uid, project_id,tmStamp)
-    print('authToken 计算 -> '+authStr)
-
+    auth = auth_token(private_key, project_uid, project_id, tm_stamp)
     # header 中是 公钥
-    header = {'X-Client-Id':publicKey}
-    urlToken = 'https://www.growingio.com/auth/token?ai={ai}&project={project}&tm={tm}&auth={auth}'.format(
+    header = {'X-Client-Id': project_key}
+    url_token = 'https://www.growingio.com/auth/token?ai={ai}&project={project}&tm={tm}&auth={auth}'.format(
         ai=project_id,
         project=project_uid,
-        tm=tmStamp,
-        auth=authStr
-        )
+        tm=tm_stamp,
+        auth=auth
+    )
     # 
-    token = req.post(urlToken,headers = header)
-    print("token 请求返回 -> "+token.text)
-    tokenReturn = json.loads(token.text)
-    print('返回 token 啦 -> '+ tokenReturn['code'])
-    return tokenReturn['code']
+    token = req.post(url_token, headers=header)
+    token = json.loads(token.text)
+    return token['code']
 
-headerEvents = {'X-Client-Id':publicKey,'Authorization':getToken()}
-print(headerEvents)
+
+headerEvents = {'X-Client-Id': project_key, 'Authorization': get_token()}
 
 def events():
     """创建事件级变量"""
-    cstmUrl = 'https://www.growingio.com/v1/api/projects/{}/vars/events'.format(project_uid)
-    with open('events.csv' if cstmFileName == '' else cstmFileName) as file:
-        trackFile = pd.read_csv(file,encoding = "utf_8",index_col=0)
-        for index,row in trackFile.iterrows():
-            cstmData={
-                'type':row['type'],
-                'description':row['description'],
-                'name':row['name'],
-                'key':row['key']
-                }
-            print('上传事件级变量: ', cstmData)
-            r = req.post(cstmUrl,headers=headerEvents,json=cstmData)
-            if (r.status_code ==200):
-                print(emoji.emojize(':thumbs_up:'),r.text)
+    cstm_url = 'https://www.growingio.com/v1/api/projects/{}/vars/events'.format(project_uid)
+    with open('events.csv' if conf.get('UploadEventsFileName','cstmFileName') == '' else 'events.csv') as file:
+        track_file = pd.read_csv(file, encoding="utf_8", index_col=0)
+        for index, row in track_file.iterrows():
+            cstm_data = {
+                'type': row['type'],
+                'description': row['description'],
+                'name': row['name'],
+                'key': row['key']
+            }
+            print('上传事件级变量: ', cstm_data)
+            r = req.post(cstm_url, headers=headerEvents, json=cstm_data)
+            if r.status_code == 200:
+                print(r.text)
             else:
-                print(emoji.emojize(':broken_heart:'),r.status_code,r.text)
-           
-            
+                print(r.status_code, r.text)
 
-def getCstmEventsVariable():
+
+def get_cstm_events_variable():
     """获取事件级变量，为了拿到服务端分配的 id """
-    getCstmEventsUrl= 'https://www.growingio.com/v1/api/projects/{}/vars/events'.format(project_uid)
-    requests = req.get(getCstmEventsUrl,headers = headerEvents)
-    if(requests.status_code == 200):
+    get_cstm_events_url = 'https://www.growingio.com/v1/api/projects/{}/vars/events'.format(project_uid)
+    requests = req.get(get_cstm_events_url, headers=headerEvents)
+    if requests.status_code == 200:
         print('---------获取事件级变量-----------')
         events = pd.read_json(requests.text)
-        events.to_csv("getEvents.csv")
+        events.to_csv("事件.csv")
+        print('---------生成表格 CSTM_Events.csv 成功 -----------')
+    else:
+        print('---------获取事件级变量失败-----------\n')
+        print(requests.status_code,"-->",requests.text)
+
+def get_vars_events_variable():
+    """获取事件列表变量"""
+    get_cstm_events_url = 'https://www.growingio.com/v1/api/projects/{}/dim/events'.format(project_uid)
+    requests = req.get(get_cstm_events_url, headers=headerEvents)
+    if requests.status_code == 200:
+        print('---------获取事件列表-----------')
+        events = pd.read_json(requests.text)
+        events.to_csv("事件级变量.csv")
         print('---------生成表格 getEvents.csv 成功 -----------')
+    else:
+        print('---------获取事件列表失败-----------\n')
+        print(requests.status_code,"-->",requests.text)
+
+def get_page_events_variable():
+    """获取页面级变量"""
+    get_cstm_events_url = 'https://www.growingio.com/v1/api/projects/{}/vars/pages'.format(project_uid)
+    requests = req.get(get_cstm_events_url, headers=headerEvents)
+    if requests.status_code == 200:
+        print('---------获取页面级变量-----------')
+        events = pd.read_json(requests.text)
+        events.to_csv("页面级变量.csv")
+        print('---------生成表格 getEvents.csv 成功 -----------')
+    else:
+        print('---------error 获取页面级变量失败-----------\n')
+        print(requests.status_code,"-->",requests.text)
+
+def get_ppl_events_variable():
+    """获取登录用户变量"""
+    get_cstm_events_url = 'https://www.growingio.com/v1/api/projects/{}/vars/peoples'.format(project_uid)
+    requests = req.get(get_cstm_events_url, headers=headerEvents)
+    if requests.status_code == 200:
+        print('---------获取登录用户变量-----------')
+        events = pd.read_json(requests.text)
+        events.to_csv("登录用户变量.csv")
+        print('---------生成表格 登录用户变量.csv 成功 -----------')
+    else:
+        print('---------error 获取登录用户变量失败-----------\n')
+        print(requests.status_code,"-->",requests.text)
 
 
-def cstmEvents():
+
+
+def cstm_events():
     """创建打点事件"""
-    eventsUrl = 'https://www.growingio.com/v1/api/projects/{}/dim/events'.format(project_uid)
-    dictEvents = {}
+    events_url = 'https://www.growingio.com/v1/api/projects/{}/dim/events'.format(project_uid)
+    dict_events = {}
     with open('getEvents.csv') as file:
-        events = pd.read_csv(file,encoding = "utf_8",index_col=0)
-        for index,row in events.iterrows():
-            dictEvents[row['name']] = {"key": row['key'],"id": row['id'], "type": row['type'],"name": row['name']}
+        events = pd.read_csv(file, encoding="utf_8", index_col=0)
+        for index, row in events.iterrows():
+            dict_events[row['name']] = {"key": row['key'], "id": row['id'], "type": row['type'], "name": row['name']}
 
-    with open('cstmEvents.csv' if cstmEventsFileName == '' else cstmEventsFileName) as file:
-        trackFile = pd.read_csv(file,encoding = "utf_8",index_col=0)
+    with open('cstmEvents.csv' if conf.get('UploadEventsFileName','cstm_eventsFileName') == '' else 'cstm_events.csv') as file:
+        track_file = pd.read_csv(file, encoding="utf_8", index_col=0)
         attrs = []
-        cstmData=[]
-        for index,row in trackFile.iterrows():
+        cstm_data = []
+        for index, row in track_file.iterrows():
             var = row['事件级变量'].split("、")
             for key in var:
                 key = key.strip()
                 if key != '':
-                    attrs.append(dictEvents[key])
-            cstmData.append({
-                'attrs':attrs,
+                    attrs.append(dict_events[key])
+            cstm_data.append({
+                'attrs': attrs,
                 'type': 'counter' if row['类型'] == '计数器' else 'number',
-                'description':row['描述'],
-                'name':index,
-                'key':row['标识符'].strip()
+                'description': row['描述'],
+                'name': index,
+                'key': row['标识符'].strip()
             })
-        print(json.dumps(cstmData))
-        r = req.post(eventsUrl,headers=headerEvents,json=cstmData)
-        if r.status_code != 200 :
-            print("自定义事件设置失败：",r.status_code,"服务器返回：",r.text)
+        print(json.dumps(cstm_data))
+        r = req.post(events_url, headers=headerEvents, json=cstm_data)
+        if r.status_code != 200:
+            print("自定义事件设置失败：", r.status_code, "服务器返回：", r.text)
         else:
-            print(emoji.emojize(':thumbs_up:'),r.text)
+            print(r.text)
 
-def pvarEvents():
+
+def pvar_events():
     """创建页面级变量"""
-    pvarUrl = 'https://www.growingio.com/v1/api/projects/{}/vars/pages'.format(project_uid)
-    with open('pvarEvents.csv' if pvarFileName == '' else pvarFileName) as file:
-        trackFile = pd.read_csv(file,encoding = "utf_8",index_col=0)
-        for index,row in trackFile.iterrows():
-            pvarData = {
-                'description':row['description'],
-                'name':row['name'],
-                'key':row['key']
+    pvar_url = 'https://www.growingio.com/v1/api/projects/{}/vars/pages'.format(project_uid)
+    with open('pvarEvents.csv' if conf.get('UploadEventsFileName','pvarFileName') == '' else 'pvarEvents.csv') as file:
+        track_file = pd.read_csv(file, encoding="utf_8", index_col=0)
+        for index, row in track_file.iterrows():
+            pvar_data = {
+                'description': row['description'],
+                'name': row['name'],
+                'key': row['key']
             }
-            r = req.post(pvarUrl,headers=headerEvents,json=pvarData)
-            if r.status_code != 200 :
-                print("自定义事件设置失败：",r.status_code,"服务器返回：",r.text)
+            r = req.post(pvar_url, headers=headerEvents, json=pvar_data)
+            if r.status_code != 200:
+                print("自定义事件设置失败：", r.status_code, "服务器返回：", r.text)
             else:
-                print(emoji.emojize(':thumbs_up:'),r.text)
-            
+                print(r.text)
+
 
 def pplEvents():
     """创建登录用户变量"""
-    pplUrl = 'https://www.growingio.com/v1/api/projects/{}/vars/peoples'.format(project_uid)
-    with open('pplEvents.csv' if pplFileName == '' else pplFileName) as file:
-        trackFile = pd.read_csv(file,encoding = "utf_8",index_col=0)
-        for index,row in trackFile.iterrows():
-            pplData = [
+    ppl_url = 'https://www.growingio.com/v1/api/projects/{}/vars/peoples'.format(project_uid)
+    with open('pplEvents.csv' if conf.get('UploadEventsFileName','pplFileName') == '' else 'pplEvents.csv') as file:
+        track_file = pd.read_csv(file, encoding="utf_8", index_col=0)
+        for index, row in track_file.iterrows():
+            ppl_data = [
                 {
-                'description':row['description'],
-                'name':index,
-                'key':row['key'],
-                'attribution':row['attribution']
+                    'description': row['description'],
+                    'name': index,
+                    'key': row['key'],
+                    'attribution': row['attribution']
                 }
             ]
-            print(pplData)
-            r = req.post(pplUrl,headers=headerEvents,json=pplData)
-            if r.status_code != 200 :
-                print("创建登录用户变量失败：",r.status_code,"服务器返回：",r.text)
+            print(ppl_data)
+            r = req.post(ppl_url, headers=headerEvents, json=ppl_data)
+            if r.status_code != 200:
+                print("创建登录用户变量失败：", r.status_code, "服务器返回：", r.text)
             else:
-                print(emoji.emojize(':thumbs_up:'),r.text)
+                print(r.text)
 
 
-  
-# events()
-getCstmEventsVariable()
-# cstmEvents()
-# pvarEvents()
-# pplEvents()
 
+
+if(conf.getboolean('Actions', "uploadEvents")):
+    events()
+    get_cstm_events_variable()
+    cstm_events()
+    pvar_events()
+    pplEvents()
+
+if(conf.get('Actions', "getEvents")):
+    get_vars_events_variable()
+    get_cstm_events_variable()
+    get_page_events_variable()
+    get_ppl_events_variable()
