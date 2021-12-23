0b04b3df14ae2c6d4ba38c4f99fcc07d8e97080c
gio-yanruixue <82151125+gio-yanruixue@users.noreply.github.com>
2021-12-23 17:01:32 +0800

    add:集成个推推送发送获取的cid

---
+++ app/src/main/java/com/growingio/giodemo/GeTuiIntentService.java
@@ -0,0 +1,154 @@
+package com.growingio.giodemo;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
+import android.util.Log;
+
+import com.growingio.android.sdk.collection.GrowingIO;
+import com.igexin.sdk.GTIntentService;
+import com.igexin.sdk.PushConsts;
+import com.igexin.sdk.PushManager;
+import com.igexin.sdk.message.BindAliasCmdMessage;
+import com.igexin.sdk.message.FeedbackCmdMessage;
+import com.igexin.sdk.message.GTCmdMessage;
+import com.igexin.sdk.message.GTNotificationMessage;
+import com.igexin.sdk.message.GTTransmitMessage;
+import com.igexin.sdk.message.SetTagCmdMessage;
+import com.igexin.sdk.message.UnBindAliasCmdMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * 继承 GTIntentService 接收来自个推的消息, 所有消息在线程中回调, 如果注册了该服务, 则务必要在 AndroidManifest中声明, 否则无法接受消息<br>
+ * onReceiveMessageData 处理透传消息<br>
+ * onReceiveClientId 接收 cid <br>
+ * onReceiveOnlineState cid 离线上线通知 <br>
+ * onReceiveCommandResult 各种事件处理回执 <br>
+ */
+public class GeTuiIntentService extends GTIntentService {
+
+    private static final String TAG = "GetuiSdkDemo";
+
+    /**
+     * 为了观察透传数据变化.
+     */
+    private static int cnt;
+
+    @Override
+    public void onReceiveServicePid(Context context, int pid) {
+        Log.d(TAG, "onReceiveServicePid -> " + pid);
+    }
+
+    @Override
+    public void onReceiveMessageData(Context context, GTTransmitMessage msg) {
+        String appid = msg.getAppid();
+        String taskid = msg.getTaskId();
+        String messageid = msg.getMessageId();
+        byte[] payload = msg.getPayload();
+        String pkg = msg.getPkgName();
+        String cid = msg.getClientId();
+
+        Log.d(TAG, "onReceiveMessageData = 111");
+
+        // 第三方回执调用接口，actionid范围为90000-90999，可根据业务场景执行
+        boolean result = PushManager.getInstance().sendFeedbackMessage(context, taskid, messageid, 90001);
+        Log.d(TAG, "call sendFeedbackMessage = " + (result ? "success" : "failed"));
+
+        Log.d(TAG, "onReceiveMessageData -> " + "appid = " + appid + "\ntaskid = " + taskid + "\nmessageid = " + messageid + "\npkg = " + pkg
+                + "\ncid = " + cid);
+
+        if (payload == null) {
+            Log.e(TAG, "receiver payload = null");
+        } else {
+            String data = new String(payload);
+            Log.d(TAG, "receiver payload = " + data);
+
+        }
+
+        Log.d(TAG, "----------------------------------------------------------------------------------------------");
+    }
+
+    @Override
+    public void onReceiveClientId(Context context, String clientid) {
+        Log.e(TAG, "onReceiveClientId -> " + "clientid = " + clientid);
+
+        try {
+            ServiceInfo info = getPackageManager().getServiceInfo(new ComponentName(this, GeTuiIntentService.class), PackageManager.GET_META_DATA);
+            GrowingIO.getInstance().setVisitor(new JSONObject().put("gio_push_getui_id", clientid));
+
+        } catch (JSONException | PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onReceiveOnlineState(Context context, boolean online) {
+        Log.d(TAG, "onReceiveOnlineState -> " + (online ? "online" : "offline"));
+    }
+
+    @Override
+    public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
+        Log.d(TAG, "onReceiveCommandResult -> " + cmdMessage);
+
+
+        int action = cmdMessage.getAction();
+
+        if (action == PushConsts.SET_TAG_RESULT) {
+            setTagResult((SetTagCmdMessage) cmdMessage);
+        } else if (action == PushConsts.BIND_ALIAS_RESULT) {
+            bindAliasResult((BindAliasCmdMessage) cmdMessage);
+        } else if (action == PushConsts.UNBIND_ALIAS_RESULT) {
+            unbindAliasResult((UnBindAliasCmdMessage) cmdMessage);
+        } else if ((action == PushConsts.THIRDPART_FEEDBACK)) {
+            feedbackResult((FeedbackCmdMessage) cmdMessage);
+        }
+    }
+
+    @Override
+    public void onNotificationMessageArrived(Context context, GTNotificationMessage message) {
+        Log.d(TAG, "onNotificationMessageArrived -> " + "appid = " + message.getAppid() + "\ntaskid = " + message.getTaskId() + "\nmessageid = "
+                + message.getMessageId() + "\npkg = " + message.getPkgName() + "\ncid = " + message.getClientId() + "\ntitle = "
+                + message.getTitle() + "\ncontent = " + message.getContent());
+    }
+
+    @Override
+    public void onNotificationMessageClicked(Context context, GTNotificationMessage message) {
+        Log.d(TAG, "onNotificationMessageClicked -> " + "appid = " + message.getAppid() + "\ntaskid = " + message.getTaskId() + "\nmessageid = "
+                + message.getMessageId() + "\npkg = " + message.getPkgName() + "\ncid = " + message.getClientId() + "\ntitle = "
+                + message.getTitle() + "\ncontent = " + message.getContent());
+
+
+    }
+
+    private void setTagResult(SetTagCmdMessage setTagCmdMsg) {
+        String sn = setTagCmdMsg.getSn();
+        String code = setTagCmdMsg.getCode();
+    }
+
+    private void bindAliasResult(BindAliasCmdMessage bindAliasCmdMessage) {
+        String sn = bindAliasCmdMessage.getSn();
+        String code = bindAliasCmdMessage.getCode();
+    }
+
+    private void unbindAliasResult(UnBindAliasCmdMessage unBindAliasCmdMessage) {
+        String sn = unBindAliasCmdMessage.getSn();
+        String code = unBindAliasCmdMessage.getCode();
+    }
+
+
+    private void feedbackResult(FeedbackCmdMessage feedbackCmdMsg) {
+        String appid = feedbackCmdMsg.getAppid();
+        String taskid = feedbackCmdMsg.getTaskId();
+        String actionid = feedbackCmdMsg.getActionId();
+        String result = feedbackCmdMsg.getResult();
+        long timestamp = feedbackCmdMsg.getTimeStamp();
+        String cid = feedbackCmdMsg.getClientId();
+
+        Log.d(TAG, "onReceiveCommandResult -> " + "appid = " + appid + "\ntaskid = " + taskid + "\nactionid = " + actionid + "\nresult = " + result
+                + "\ncid = " + cid + "\ntimestamp = " + timestamp);
+    }
+}
---
+++ app/src/main/java/com/growingio/giodemo/GeTuiPushService.java
@@ -0,0 +1,7 @@
+package com.growingio.giodemo;
+
+/**
+ * 此类空实现即可.
+ */
+public class GeTuiPushService extends com.igexin.sdk.PushService {
+}
